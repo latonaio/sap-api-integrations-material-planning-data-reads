df692c4996d8e748955835878e996bc48d3af19e
Yurino Oga <yurino.o@latona.io>
2021-11-30 16:43:41 +0900

    release

--- SAP_API_Output_Formatter/format.go
+++ SAP_API_Output_Formatter/format.go
@@ -2,13 +2,13 @@ package sap_api_output_formatter
 
 import (
 	"encoding/json"
-	"sap-api-integrations-material-stock-reads/SAP_API_Caller/responses"
+	"sap-api-integrations-material-planning-data-reads/SAP_API_Caller/responses"
 
 	"github.com/latonaio/golang-logging-library/logger"
 )
 
-func ConvertToMaterialStock(raw []byte, l *logger.Logger) *MaterialStock {
-	pm := &responses.MaterialStock{}
+func ConvertToMaterialPlanningData(raw []byte, l *logger.Logger) *MaterialPlanningData {
+	pm := &responses.MaterialPlanningData{}
 	err := json.Unmarshal(raw, pm)
 	if err != nil {
 		l.Error(err)
@@ -23,11 +23,38 @@ func ConvertToMaterialStock(raw []byte, l *logger.Logger) *MaterialStock {
 	}
 	data := pm.D.Results[0]
 
-	return &MaterialStock{
-		Material:            data.Material,
-		Plant:               data.Plant,
-		StorageLocation:     data.StorageLocation,
-		Batch:               data.ValidityStartDate,
-		ToMaterialStock:     data.ToMaterialStock,
+	return &MaterialPlanningData{
+		MRPElementDocumentType         data.MRPElementDocumentType,
+		Material                       data.Material,
+		MRPPlant                       data.MRPPlant,
+		MRPArea                        data.MRPArea,
+		MRPPlanningSegment             data.MRPPlanningSegment,
+		MRPPlanningSegmentType         data.MRPPlanningSegmentType,
+		MRPElement                     data.MRPElement,
+		MRPElementItem                 data.MRPElementItem,
+		HasAcceptedShortage            data.HasAcceptedShortage,
+		TimeHorizonInDays              data.TimeHorizonInDays,
+		MaterialShortageDuration       data.MaterialShortageDuration,
+		MaterialShortageDurnInWorkdays data.MaterialShortageDurnInWorkdays,
+		MRPController                  data.MRPController,
+		MRPSafetyDuration              data.MRPSafetyDuration,
+		DaysOfSupplyDuration           data.DaysOfSupplyDuration,
+		MaterialBaseUnit               data.MaterialBaseUnit,
+		MRPElementAvailyOrRqmtDate     data.MRPElementAvailyOrRqmtDate,
+		MRPSafetyDurationEndDate       data.MRPSafetyDurationEndDate,
+		ProductionVersion              data.ProductionVersion,
+		StorageLocation                data.StorageLocation,
+		ExceptionMessageNumber         data.ExceptionMessageNumber,
+		ExceptionMessageNumber2        data.ExceptionMessageNumber2,
+		MRPAvailableQuantity           data.MRPAvailableQuantity,
+		MaterialShortageQuantity       data.MaterialShortageQuantity,
+		MaterialShortageEndDate        data.MaterialShortageEndDate,
+		MaterialShortageStartDate      data.MaterialShortageStartDate,
+		MRPElementOpenQuantity         data.MRPElementOpenQuantity,
+		MRPElementQuantityIsFirm       data.MRPElementQuantityIsFirm,
+		MaterialSafetyStockQty         data.MaterialSafetyStockQty,
+		MRPElementBusinessPartner      data.MRPElementBusinessPartner,
+		MRPElementIsPartiallyDelivered data.MRPElementIsPartiallyDelivered,
+		MRPElementIsReleased           data.MRPElementIsReleased,
 	}
 }
