8c274da4af72cc8f9e47bdd5f20b1ffc4a255902
Yurino Oga <yurino.o@latona.io>
2021-11-30 14:50:24 +0900

    release

--- SAP_API_Input_Reader/type.go
+++ SAP_API_Input_Reader/type.go
@@ -1,4 +1,4 @@
-package file_reader
+package sap_api_input_reader
 
 type EC_MC struct {
 	ConnectionKey string      `json:"connection_key"`
@@ -8,17 +8,17 @@ type EC_MC struct {
 	MRPElement    struct {
 		MRPElement                     string      `json:"document_no"`
 		DeliverTo                      string      `json:"deliver_to"`
-		MRPAvailableQuantity           float64     `json:"quantity"`
-		PickedQuantity                 float64     `json:"picked_quantity"`
-		Price                          float64     `json:"price"`
+		MRPAvailableQuantity           string      `json:"quantity"`
+		PickedQuantity                 string      `json:"picked_quantity"`
+		Price                          string      `json:"price"`
 	    Batch                          string      `json:"batch"`
 	} `json:"document"`
 	ProductionOrder struct {
 		DocumentNo           string      `json:"document_no"`
 		Status               string      `json:"status"`
 		DeliverTo            string      `json:"deliver_to"`
-		Quantity             float64     `json:"quantity"`
-		CompletedQuantity    float64     `json:"completed_quantity"`
+		Quantity             string      `json:"quantity"`
+		CompletedQuantity    string      `json:"completed_quantity"`
 	    PlannedStartDate     string      `json:"planned_start_date"`
 	    PlannedValidatedDate string      `json:"planned_validated_date"`
 	    ActualStartDate      string      `json:"actual_start_date"`
@@ -26,11 +26,11 @@ type EC_MC struct {
 	    Batch                string      `json:"batch"`
 		Work              struct {
 			WorkNo                   string      `json:"work_no"`
-			Quantity                 float64     `json:"quantity"`
-			CompletedQuantity        float64     `json:"completed_quantity"`
-			ErroredQuantity          float64     `json:"errored_quantity"`
+			Quantity                 string      `json:"quantity"`
+			CompletedQuantity        string      `json:"completed_quantity"`
+			ErroredQuantity          string      `json:"errored_quantity"`
 			Component                string      `json:"component"`
-			PlannedComponentQuantity float64     `json:"planned_component_quantity"`
+			PlannedComponentQuantity string      `json:"planned_component_quantity"`
 			PlannedStartDate         string      `json:"planned_start_date"`
 			PlannedStartTime         string      `json:"planned_start_time"`
 			PlannedValidatedDate     string      `json:"planned_validated_date"`
@@ -44,12 +44,12 @@ type EC_MC struct {
 	APISchema                      string      `json:"api_schema"`
 	MaterialCode                   string      `json:"material_code"`
 	MRPPlant                       string      `json:"plant/supplier"`
-	Stock                          float64     `json:"stock"`
+	Stock                          string      `json:"stock"`
 	MRPElementDocumentType         string      `json:"document_type"`
 	MRPElement                     string      `json:"document_no"`
 	MRPElementAvailyOrRqmtDate     string      `json:"planned_date"`
 	ValidatedDate                  string      `json:"validated_date"`
-	Deleted                        string      `json:"deleted"`
+	Deleted                        bool        `json:"deleted"`
 }
 
 type SDC struct {
@@ -67,12 +67,12 @@ type SDC struct {
 		MRPElement                     string      `json:"MRPElement"`
 		MRPElementItem                 string      `json:"MRPElementItem"`
 		HasAcceptedShortage            string      `json:"HasAcceptedShortage"`
-		TimeHorizonInDays              int         `json:"TimeHorizonInDays"`
-		MaterialShortageDuration       int         `json:"MaterialShortageDuration"`
-		MaterialShortageDurnInWorkdays int         `json:"MaterialShortageDurnInWorkdays"`
+		TimeHorizonInDays              string      `json:"TimeHorizonInDays"`
+		MaterialShortageDuration       string      `json:"MaterialShortageDuration"`
+		MaterialShortageDurnInWorkdays string      `json:"MaterialShortageDurnInWorkdays"`
 		MRPController                  string      `json:"MRPController"`
-		MRPSafetyDuration              float64     `json:"MRPSafetyDuration"`
-		DaysOfSupplyDuration           int         `json:"DaysOfSupplyDuration"`
+		MRPSafetyDuration              string      `json:"MRPSafetyDuration"`
+		DaysOfSupplyDuration           string      `json:"DaysOfSupplyDuration"`
 		MaterialBaseUnit               string      `json:"MaterialBaseUnit"`
 		MRPElementAvailyOrRqmtDate     string      `json:"MRPElementAvailyOrRqmtDate"`
 		MRPSafetyDurationEndDate       string      `json:"MRPSafetyDurationEndDate"`
@@ -80,19 +80,19 @@ type SDC struct {
 		StorageLocation                string      `json:"StorageLocation"`
 		ExceptionMessageNumber         string      `json:"ExceptionMessageNumber"`
 		ExceptionMessageNumber2        string      `json:"ExceptionMessageNumber2"`
-		MRPAvailableQuantity           float64     `json:"MRPAvailableQuantity"`
-		MaterialShortageQuantity       float64     `json:"MaterialShortageQuantity"`
+		MRPAvailableQuantity           string      `json:"MRPAvailableQuantity"`
+		MaterialShortageQuantity       string      `json:"MaterialShortageQuantity"`
 		MaterialShortageEndDate        string      `json:"MaterialShortageEndDate"`
 		MaterialShortageStartDate      string      `json:"MaterialShortageStartDate"`
-		MRPElementOpenQuantity         float64     `json:"MRPElementOpenQuantity"`
-		MRPElementQuantityIsFirm       string      `json:"MRPElementQuantityIsFirm"`
-		MaterialSafetyStockQty         float64     `json:"MaterialSafetyStockQty"`
+		MRPElementOpenQuantity         string      `json:"MRPElementOpenQuantity"`
+		MRPElementQuantityIsFirm       bool        `json:"MRPElementQuantityIsFirm"`
+		MaterialSafetyStockQty         string      `json:"MaterialSafetyStockQty"`
 		MRPElementBusinessPartner      string      `json:"MRPElementBusinessPartner"`
-		MRPElementIsPartiallyDelivered string      `json:"MRPElementIsPartiallyDelivered"`
-		MRPElementIsReleased           string      `json:"MRPElementIsReleased"`
+		MRPElementIsPartiallyDelivered bool        `json:"MRPElementIsPartiallyDelivered"`
+		MRPElementIsReleased           bool        `json:"MRPElementIsReleased"`
 	} `json:"MaterialPlanningData"`
 	APISchema    string `json:"api_schema"`
 	MaterialCode string `json:"material_code"`
 	MRPPlant     string `json:"plant"`
-	Deleted      string `json:"deleted"`
+	Deleted      bool   `json:"deleted"`
 }
---
+++ SAP_API_Output_Formatter/type.go
@@ -0,0 +1,47 @@
+package sap_api_output_formatter
+
+type MaterialPlanningDataReads struct {
+	ConnectionKey string `json:"connection_key"`
+	Result        bool   `json:"result"`
+	RedisKey      string `json:"redis_key"`
+	Filepath      string `json:"filepath"`
+	APISchema     string `json:"api_schema"`
+	MaterialCode  string `json:"material_code"`
+	MRPPlant      string `json:"plant"`
+	Deleted       bool   `json:"deleted"`
+}
+
+type MaterialPlanningData struct {
+	MRPElementDocumentType         string `json:"MRPElementDocumentType"`
+	Material                       string `json:"Material"`
+	MRPPlant                       string `json:"MRPPlant"`
+	MRPArea                        string `json:"MRPArea"`
+	MRPPlanningSegment             string `json:"MRPPlanningSegment"`
+	MRPPlanningSegmentType         string `json:"MRPPlanningSegmentType"`
+	MRPElement                     string `json:"MRPElement"`
+	MRPElementItem                 string `json:"MRPElementItem"`
+	HasAcceptedShortage            string `json:"HasAcceptedShortage"`
+	TimeHorizonInDays              string `json:"TimeHorizonInDays"`
+	MaterialShortageDuration       string `json:"MaterialShortageDuration"`
+	MaterialShortageDurnInWorkdays string `json:"MaterialShortageDurnInWorkdays"`
+	MRPController                  string `json:"MRPController"`
+	MRPSafetyDuration              string `json:"MRPSafetyDuration"`
+	DaysOfSupplyDuration           string `json:"DaysOfSupplyDuration"`
+	MaterialBaseUnit               string `json:"MaterialBaseUnit"`
+	MRPElementAvailyOrRqmtDate     string `json:"MRPElementAvailyOrRqmtDate"`
+	MRPSafetyDurationEndDate       string `json:"MRPSafetyDurationEndDate"`
+	ProductionVersion              string `json:"ProductionVersion"`
+	StorageLocation                string `json:"StorageLocation"`
+	ExceptionMessageNumber         string `json:"ExceptionMessageNumber"`
+	ExceptionMessageNumber2        string `json:"ExceptionMessageNumber2"`
+	MRPAvailableQuantity           string `json:"MRPAvailableQuantity"`
+	MaterialShortageQuantity       string `json:"MaterialShortageQuantity"`
+	MaterialShortageEndDate        string `json:"MaterialShortageEndDate"`
+	MaterialShortageStartDate      string `json:"MaterialShortageStartDate"`
+	MRPElementOpenQuantity         string `json:"MRPElementOpenQuantity"`
+	MRPElementQuantityIsFirm       bool   `json:"MRPElementQuantityIsFirm"`
+	MaterialSafetyStockQty         string `json:"MaterialSafetyStockQty"`
+	MRPElementBusinessPartner      string `json:"MRPElementBusinessPartner"`
+	MRPElementIsPartiallyDelivered bool   `json:"MRPElementIsPartiallyDelivered"`
+	MRPElementIsReleased           bool   `json:"MRPElementIsReleased"`
+}
