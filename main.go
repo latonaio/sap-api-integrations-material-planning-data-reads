5c11d66f1a5f13593675fa25b50fef45ae7dac52
Yurino Oga <yurino.o@latona.io>
2021-12-06 09:04:32 +0900

    release

--- main.go
+++ main.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    sap_api_caller "sap-api-integrations-material-planning-data-reads/SAP_API_Caller"
-    "sap-api-integrations-material-planning-data-reads/file_reader"
+	sap_api_caller "sap-api-integrations-material-planning-data-reads/SAP_API_Caller"
+	"sap-api-integrations-planned-material-planning-data-reads/sap_api_input_reader"
 
-    "github.com/latonaio/golang-logging-library/logger"
+	"github.com/latonaio/golang-logging-library/logger"
 )
 
 func main() {
-    l := logger.NewLogger()
-    fr := file_reader.NewFileReader()
-    inoutSDC := fr.ReadSDC("./Inputs//SDC_Material_Planning_Data_sample.json")
-    caller := sap_api_caller.NewSAPAPICaller(
-        "https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
-    )
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs//SDC_Material_Planning_Data_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
+	)
 
     caller.AsyncGetMaterialPlanningData(
-        inoutSDC.MRPElementDocumentType.Material.MRPPlant,
+        inoutSDC.MaterialPlanningData.MRPElementDocumentType,
+        inoutSDC.MaterialPlanningData.Material,
+        inoutSDC.MaterialPlanningData.MRPPlant,
     )
 }
