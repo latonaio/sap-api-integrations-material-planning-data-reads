43e206acc5aabc0e036f42bff3f72859fcc7087b
Yurino Oga <yurino.o@latona.io>
2022-04-28 16:15:59 +0900

    release

--- main.go
+++ main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-    sap_api_caller "sap-api-integrations-material-planning-data-reads/SAP_API_Caller"
-    "sap-api-integrations-material-planning-data-reads/file_reader"
+	sap_api_caller "sap-api-integrations-material-planning-data-reads/SAP_API_Caller"
+	sap_api_input_reader "sap-api-integrations-material-planning-data-reads/SAP_API_Input_Reader"
 
-    "github.com/latonaio/golang-logging-library/logger"
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
 
-accepter := inoutSDC.Accepter
-if len(accepter) == 0 || accepter[0] == "All" {
-    accepter = []string{
-        "MaterialPlanningData",
-    }
-}
-
 func main() {
-    l := logger.NewLogger()
-    fr := file_reader.NewFileReader()
-    inoutSDC := fr.ReadSDC("./Inputs//SDC_Material_Planning_Data_sample.json")
-    caller := sap_api_caller.NewSAPAPICaller(
-        "https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
-    )
+	l := logger.NewLogger()
+	fr := sap_api_input_reader.NewFileReader()
+	inoutSDC := fr.ReadSDC("./Inputs/SDC_Material_Planning_Data_sample.json")
+	caller := sap_api_caller.NewSAPAPICaller(
+		"https://sandbox.api.sap.com/s4hanacloud/sap/opu/odata/sap/", l,
+	)
 
-    caller.AsyncGetMaterialPlanningData(
-        inoutSDC.MRPElementDocumentType.Material.MRPPlant,
-        accepter,
-    )
+	accepter := inoutSDC.Accepter
+	if len(accepter) == 0 || accepter[0] == "All" {
+		accepter = []string{
+			"MaterialPlanningData",
+		}
+	}
+
+	caller.AsyncGetMaterialPlanningData(
+		inoutSDC.MaterialPlanningData.MRPElementDocumentType,
+		inoutSDC.MaterialPlanningData.Material,
+		inoutSDC.MaterialPlanningData.MRPPlant,
+		accepter,
+	)
 }
